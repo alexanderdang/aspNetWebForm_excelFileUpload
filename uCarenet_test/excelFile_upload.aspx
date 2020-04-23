8d286de61b890b3ac83e3f79cc697f232370a58c
Alexander Dang <dang.alexander@gmail.com>
2020-04-22 22:52:14 -0400

    Add project files.

---
+++ uCarenet_test/excelFile_upload.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="excelFile_upload.aspx.cs" Inherits="uCarenet_test.excelFile_upload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8" />
+    <title>uCarenet Test Excel File Upload Web Page</title>
+    <meta name="description" content="Excel File Upload" />
+</head>
+<body>
+    <h1>Excel File Upload</h1>
+    <form id="excelFile_upload_form" runat="server">
+        <div>
+            <asp:FileUpload ID="excelFileUpload" runat="server" />
+            <asp:Button id="btnSubmit" OnClick="btnSubmit_Click" runat="server" Text="Upload" />
+            <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
+        </div>
+        <div>
+            <asp:GridView ID="excelFileGridView" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ uCarenet_test/excelFile_upload.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data.OleDb;
+using System.Configuration;
+using System.IO;
+using System.Data;
+
+namespace uCarenet_test
+{
+    public partial class excelFile_upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Retrieve path from Web.config file to upload excel file
+            string filePath = ConfigurationManager.AppSettings["filePath"].ToString();
+
+            if (excelFileUpload.HasFile)
+            {
+                try
+                {
+                    string[] allowedFileTypes = { ".xls", ".xlsx" };
+                    excelFileUpload.SaveAs(filePath + excelFileUpload.FileName);
+                    string fileExtension = System.IO.Path.GetExtension(excelFileUpload.PostedFile.FileName);
+
+                    //bool isFileValid = allowedFileTypes.Contains(fileExtension);
+
+                    OleDbConnection excelFileConnection = null;
+                    if (fileExtension == ".xls")
+                    {
+                        excelFileConnection = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + "; Extended Properties=Excel 8.0;");
+                    }
+                    else if (fileExtension == ".xlsx")
+                    {
+                        excelFileConnection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + "; Extended Properties=Excel 12.0;");
+                    }
+                    excelFileConnection.Open();
+
+                    DataTable dt = excelFileConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    string getExcelSheetName = dt.Rows[0]["Table Name"].ToString;
+
+                    excelFileGridView.DataBind();
+                    
+
+                } catch (Exception error)
+                {
+                    lblMessage.Text = error.Message;
+                }
+
+            }
+        }
+    }
+}
